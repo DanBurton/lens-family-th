60d1a3083547cdaf59c78a8dcb3e767cf104b434
Dan Burton <danburton.email@gmail.com>
2021-11-10 18:22:20 -0500

    support th 2.18, closes #19

--- src/Lens/Family/THCore.hs
+++ src/Lens/Family/THCore.hs
@@ -158,7 +158,11 @@ deriveTraversal nameTransform ty cs con = do
 
 deconstructReconstruct :: Con -> String -> (Pat, Exp)
 deconstructReconstruct c nameBase = (pat, expr) where
-  pat = ConP conN (map VarP argNames)
+#if MIN_VERSION_template_haskell(2,18,0)
+  pat = ConP conN [] (map VarP argNames)
+#else
+  pat = ConP conN    (map VarP argNames)
+#endif
   expr = foldl AppE (ConE conN) (map VarE argNames)
   (conN, nArgs) = getConInfo c
   argNames = mkArgNames nArgs nameBase
